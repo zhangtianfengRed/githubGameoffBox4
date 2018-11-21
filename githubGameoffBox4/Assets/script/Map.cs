8f68434064a07496c021d3c667d57071fb2dc6e0
Red-zhangtianfeng <13529812388@163.com>
2018-11-22 01:32:58 +0800

    基础框架搭设

---
+++ githubGameoffBox4/Assets/script/Map.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    public int high;
+    public int width;
+    public GameObject ground;
+    private GameObject GROUND;
+    public GameObject Boxgroup;
+    public GameObject Targetgroup;
+    private MapData[,] data;
+	// Use this for initialization
+	void Start () {
+        data = new MapData[width, high];
+
+        GROUND = new GameObject("GROUND");
+        mapStart();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #region 加载关卡
+    public interface load
+    {
+        void loadlevel(int level);
+    }
+
+    class levelload : load
+    {
+
+        public void loadlevel(int level)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(level+1);
+        }
+    }
+    public interface level
+    {
+        void Done();
+    }
+    public class Level:level
+    {
+        public GameObject mylevel { get; set; }
+        private load Load;
+        public int _level { get; set; }
+
+        public Level(load _load)
+        {
+            this.Load = _load;
+        }
+
+        public void Done()
+        {
+            Load.loadlevel(_level);
+        }
+    }
+    #endregion
+   
+
+    void mapStart()
+    {
+        for (int x = 0; x < high; x++) {
+            for(int y = 0; y < width; y++)
+            {
+                GameObject T= Instantiate(ground, new Vector3Int(x, y, 0), Quaternion.identity);
+                T.transform.parent = GROUND.transform;
+            }
+        }
+    }
+
+    void mapDataload(GameObject T)
+    {
+        foreach(Transform i in T.transform)
+        {
+            if (i != null)
+            {
+                Vector2Int iVector = new Vector2Int((int)i.position.x, (int)i.position.y);
+                data[iVector.x, iVector.y].Boxtag(i.gameObject);
+            }
+        }
+    }
+    class MapData
+    {
+        public GameObject Box { get; set; }
+        public GameObject Target { get; set; }
+
+        public void Boxtag(GameObject gameObject)
+        {
+            switch (gameObject.transform.tag)
+            {
+                case ("box"):
+                    if (Box != null) { Box = gameObject; }                    
+                    break;
+                case ("target"):
+                    if (Target != null) { Target = gameObject; }                  
+                    break;
+            }
+        }
+
+        protected void BoxClean()
+        {
+            Box = null;
+        }
+        protected void TargetClean()
+        {
+            Target = null;
+        }
+        protected void ALLClean()
+        {
+            Box = null;
+            Target = null;
+        }
+        
+        
+    }
+
+
+    
+}
---
+++ githubGameoffBox4/Assets/script/playInput.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playInput : MonoBehaviour {
+    ControlObject controlObject;
+    Box ghost;
+    public GameObject GhostGameobject;
+    Receiver receiver = new Receiver();
+    Commad commadup;
+    Commad commadleft;
+    Commad commadright;
+    Commad commaddown;
+    // Use this for initialization
+    void Start () {
+        ghost = new Ghost(GhostGameobject);
+        controlObject = new ControlObject(ghost);
+        commadup = new Moveup(receiver);
+        commadleft = new Moveleft(receiver);
+        commadright = new MoveRight(receiver);
+        commaddown = new MoveDown(receiver);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commadup);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commadleft);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commaddown);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commadright);
+            InvokeCommad.Excutecommad();
+        }
+    }
+    #region 控制状态的枚举
+    enum Control
+    {
+        Ghost,
+        Box
+    }
+    #endregion
+
+    public abstract class Box
+    {
+        protected GameObject Object;
+        public Box(GameObject gameObject)
+        {
+            Object = gameObject;
+        }
+        public GameObject UsegameObject
+        {
+            get
+            {
+                return Object;
+            }
+        }
+    }
+
+    public class Ghost : Box
+    {
+        public Ghost(GameObject gameObject):base(gameObject) { }
+    }
+
+    public class BoxObject : Box
+    {
+        public BoxObject(GameObject gameObject) : base(gameObject) { }
+    }
+
+    public class ControlObject
+    {
+        public Box Box;
+        public ControlObject(Box box)
+        {
+            Box = box;
+        }
+        public GameObject UseObject()
+        {
+            return Box.UsegameObject;
+        }
+    }
+
+
+    #region 调用对象
+    public class InvokeCommad
+    {
+        public Commad Commad;
+        public InvokeCommad(Commad commad)
+        {
+            Commad = commad;
+        }
+        public void Excutecommad()
+        {
+            Commad.Action();
+        }
+    }
+    #endregion
+
+    #region 行为方法
+    public class Receiver
+    {
+        public void up()
+        {
+            Debug.Log("up");
+        }
+        public void Down()
+        {
+            Debug.Log("down");
+        }
+        public void left()
+        {
+            Debug.Log("left");
+        }
+        public void right()
+        {
+            Debug.Log("right");
+        }
+    }
+    #endregion 
+    #region 行为对象化 
+    public abstract class Commad
+    {
+        protected Receiver PlayRecever;
+
+        public Commad(Receiver playRecever )
+        {
+            PlayRecever = playRecever;
+        }
+        public virtual void Action() { }
+    }
+    public class MoveDown : Commad
+    {
+        public MoveDown(Receiver receiver) : base(receiver) { }
+        public override void Action()
+        {
+            PlayRecever.Down();
+        }
+    }
+    public class Moveup : Commad
+    {
+        public Moveup(Receiver receiver) : base(receiver) { }
+        public override void Action()
+        {
+            PlayRecever.up();
+        }
+    }
+    public class Moveleft : Commad
+    {
+        public Moveleft(Receiver receiver) : base(receiver) { }
+        public override void Action()
+        {
+            PlayRecever.left();
+        }
+    }
+    public class MoveRight : Commad
+    {
+        public MoveRight(Receiver receiver) : base(receiver) { }
+        public override void Action()
+        {
+            PlayRecever.right();
+        }
+    }
+    #endregion
+}
